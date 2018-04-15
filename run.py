47797d1ca780cf48fe7332d775258d75dc8dbcb7
Anish <anishnavelkar24@gmail.com>
2018-04-15 14:45:51 +0530

    add files

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
 from flask import Flask, send_from_directory, request, Response
+import pymongo
+from pymongo import MongoClient
+import re
+client=MongoClient('localhost',27017)
+db=client.tvb_amazon
 
 app = Flask('mini-amazon', static_url_path='')
-product_list = []
-
 
 @app.route('/health', methods=['GET'])
 def health():
@@ -16,24 +19,48 @@ def index():
 
 @app.route('/api/products', methods=['POST', 'GET'])
 def products():
-    if request.method == 'GET':
-        print(product_list)
-        print(request.args['name'])
-        for product in product_list:
-            if product['name'] == request.args['name']:
-                return Response(str(product), 200)
-        return Response(str({}), 200)
+    if request.method=='GET':
+        query= {
+            'name': re.compile(request.args['name'],re.IGNORECASE)
+
+            }
+
+        matching_prods=db.products.find(query)
+        matches=[]
+        for prod in matching_prods:
+            matches.append(prod)
+
+        return Response(str(matches),mimetype='application/json',status=200)
+
     elif request.method == 'POST':
-        product = dict()
-        product['name'] = request.form['name']
-        product['description'] = request.form['description']
-        product['price'] = request.form['price']
-        print(product)
+        if request.form['op_type']=="insert":
+            product = dict()
+            product['name'] = request.form['name']
+            product['description'] = request.form['description']
+            product['price'] = request.form['price']
+            db.products.insert_one(product)
+            return Response(str({'status':'success'}),mimetype='application/json',status= 200)
+        elif request.form['op_type']=="delete":
+            name = request.form['name']
+            db.products.delete_one(filter={'name':name})
+            return Response(str({'status': 'success'}), mimetype='application/json', status=200)
+        elif request.form['op_type'] == "update":
+            condition = dict()
+            condition['name'] = request.form['cur_name']
 
-        product_list.append(product)
+            replacement=dict()
+            if request.form['name']!='':
+                replacement['name'] = request.form['name']
+            if request.form['description']!='':
+                replacement['desc'] = request.form['description']
+            if request.form['price']!='':
+                replacement['price'] = request.form['price']
+            db.products.update_one(filter=condition, update={'$set':replacement})
+            return Response(str({'status':'updated'}),mimetype='application/json',status=200)
 
-        return Response('OK', 200)
+
+
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000,debug=True)
